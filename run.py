fb8d897559adac82d5e5f881cf38c7ba24f55822
Eddy-Arch <bishclapper69420@gmail.com>
2020-07-18 07:19:05 +0000

    added command line args

--- run.py
+++ run.py
@@ -11,15 +11,43 @@ from discord.ext.commands import has_permissions, CheckFailure
 import sys
 import os
 import string
+import argparse
 
-##commited from arch linux belive it or not i know this is mega swagg innit
+# commited from arch linux belive it or not i know this is mega swagg innit
 from colorama import init
+## argsparse ###
+description = 'A simple bruteforce script that attempts to guess discord server invite codes.'
+# Initiate the parser with a description
+parser = argparse.ArgumentParser(description=description)
+parser.add_argument("-v", "--version",
+                    help="show program version", action="store_true")
+parser.add_argument("-c", "--commands",
+                    help="lists available commands", action="store_true")
+args = parser.parse_args()
+
+
+if args.version:
+    print("V1.0")
+    exit()
+
+if args.commands:
+    print("List of available commands:")
+    print("  +guess <amount of guesses to make>")
+    print("  +help")
+    exit()
+
+
+###/ARGSPARSE###
+
 
 init()
-#set the preifx and disable the builtin help command that comes with discord.py
+# set the preifx and disable the builtin help command that comes with discord.py
 client = commands.Bot(command_prefix="+")
 client.remove_command("help")
 
+
+print(Fore.WHITE + "[" + Fore.RED + "+" +
+      Fore.WHITE + "]" + "Welcome home, script kiddie")
 print(
     Fore.WHITE + "[" + Fore.BLUE + '+' + Fore.WHITE + "]" + Fore.BLUE + " attempting to establish connection to the client")
 
@@ -28,7 +56,7 @@ print(
 async def on_ready():
     print("")
 
-    
+
 @client.command()
 async def help(ctx):
     author = ctx.message.author
@@ -37,9 +65,12 @@ async def help(ctx):
         colour=discord.Color.purple()
     )
     embed.set_author(name="Help")
-    embed.add_field(name="+help", value='shows this help message', inline=False)
-    embed.add_field(name="+guess", value='guesses guild invites. usage= +guess (number of guesses.)', inline=False)
-    embed.add_field(name="Github", value='this discord bots source code is available on github. heres a link:', inline=False)
+    embed.add_field(
+        name="+help", value='shows this help message', inline=False)
+    embed.add_field(
+        name="+guess", value='guesses guild invites. usage= +guess (number of guesses.)', inline=False)
+    embed.add_field(
+        name="Github", value='this discord bots source code is available on github. heres a link:', inline=False)
     embed.add_field(name="+clear", value='clears the console', inline=False)
     await ctx.send(embed=embed)
 
@@ -68,9 +99,9 @@ async def on_ready():
         Fore.WHITE + "[" + Fore.GREEN + '+' + Fore.WHITE + "]" + Fore.GREEN + " connection established, logged in as: " + client.user.name)
 
 
-####progress bar
+# progress bar
 
-def progressBar(iterable, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+def progressBar(iterable, prefix='', suffix='', decimals=1, length=100, fill='█', printEnd="\r"):
     """
     Call in a loop to create terminal progress bar
     @params:
@@ -85,11 +116,13 @@ def progressBar(iterable, prefix = '', suffix = '', decimals = 1, length = 100, 
     """
     total = len(iterable)
     # Progress Bar Printing Function
-    def printProgressBar (iteration):
-        percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+
+    def printProgressBar(iteration):
+        percent = ("{0:." + str(decimals) + "f}").format(100 *
+                                                         (iteration / float(total)))
         filledLength = int(length * iteration // total)
         bar = fill * filledLength + '-' * (length - filledLength)
-        print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
+        print(f'\r{prefix} |{bar}| {percent}% {suffix}', end=printEnd)
     # Initial Call
     printProgressBar(0)
     # Update Progress Bar
@@ -100,15 +133,9 @@ def progressBar(iterable, prefix = '', suffix = '', decimals = 1, length = 100, 
     print()
 
 
-
-
-
-
-
-
-
 def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
-     return ''.join(random.choice(chars) for _ in range(size))
+    return ''.join(random.choice(chars) for _ in range(size))
+
 
 @client.command()
 async def guess(ctx, reason="None"):
@@ -126,26 +153,24 @@ async def guess(ctx, reason="None"):
     b = int(reason)
     i = 0
 
-    for item in progressBar(items, prefix = 'Progress:', suffix = 'Complete', length = 50):
+    for item in progressBar(items, prefix='Progress:', suffix='Complete', length=50):
         await ctx.send("discord.gg/" + id_generator(6, "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM123456789"))
-        #time.sleep(0.1)
+        # time.sleep(0.1)
+
+    # while(i < b):
+    #   await ctx.send("discord.gg/" + id_generator(6, "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM123456789"))
+    #   i+= 1
+        # time.sleep(1)
 
 
-    #while(i < b):
-     #   await ctx.send("discord.gg/" + id_generator(6, "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM123456789"))
-     #   i+= 1
-        #time.sleep(1)
-
- 
-
-###clear the console command
+# clear the console command
 @client.command()
 async def clear(ctx):
     os.system("clear")
 # dummy token in here, well its a dummy now. appearantly discord has a web crawler that found my bots token in here. pretty damn cool.
 client.run("NzMxMjU5MzI5NzQyODk3MjA0.Xwmj3w.MmxsRzifBc-WzwIDhRS_ZcIXlWA")
 
-#br
+# br
 # this is the animation that gets played in case of a crash, error, dyno error etc. if you are running this from windows, i recommend replacing "clear" with "cls" to avoid a visual bug, reminding you that the clear command is unix like only. the t == 3 LOC means the amount of times the animation will repeat before terminating the application.
 t = 0
 while t != 10:
@@ -158,7 +183,7 @@ while t != 10:
     print("died..")
     time.sleep(1)
     os.system("clear")
-    print("died...") 
+    print("died...")
     time.sleep(1)
     os.system("clear")
     print("died..")
@@ -167,7 +192,6 @@ while t != 10:
     print("died.")
     time.sleep(1)
     os.system("clear")
-    t+= 1
+    t += 1
     if(t == 3):
         exit()
-
